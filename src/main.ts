38a2f5e166ca9387ccd5390e0c8129b0186e079f
Sylvester Kwon <sylvesterkwon@gmail.com>
2023-06-05 19:38:46 +0900

    feat: Add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Dayjs config
   dayjs.extend(utc);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Recrit API')
+    .setDescription('Recrit API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
