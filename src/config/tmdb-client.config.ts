feae0a584ff33f3583630b19f54c56885fdc8f80
Sylvester Kwon <sylvesterkwon@gmail.com>
2023-09-17 12:56:41 +0900

    fix: Change property name "access_token" to "accessToken"

--- src/config/tmdb-client.config.ts
+++ src/config/tmdb-client.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('tmdbClient', () => ({
   tmdbApiKey: process.env.TMDB_API_KEY,
-  tmdbApiReadAccessToken: process.env.TMDB_API_READ_ACCESS_TOKEN,
+  tmdbApiReadAccessToken: process.env.TMDB_API_READ_accessToken,
 }));
