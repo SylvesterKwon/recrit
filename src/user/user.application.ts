787e10e0d576a5fe1f30c7a5207a191732bec834
Sylvester Kwon <sylvesterkwon@gmail.com>
2023-09-24 10:41:42 +0900

    feat: Add User and UserId decorator

--- src/user/user.application.ts
+++ src/user/user.application.ts
@@ -3,8 +3,9 @@ import { SignUpDto } from './dto/sign-up.dto';
 import { MikroORM } from '@mikro-orm/core';
 import { UserService } from './user.service';
 import { AuthService } from './auth.service';
-import { User } from './entities/user.entity';
 import { Transactional } from 'src/common/decorators/transactional.decorator';
+import { UserRepository } from './repositories/user.repository';
+import { UserNotFoundException } from 'src/common/exceptions/user.exception';
 
 @Injectable()
 export class UserApplication {
@@ -12,10 +13,14 @@ export class UserApplication {
     private orm: MikroORM,
     private userService: UserService,
     private authService: AuthService,
+    private userRepository: UserRepository,
   ) {}
 
   @Transactional()
-  async signIn(user: User) {
+  async signIn(userId: number) {
+    const user = await this.userRepository.findById(userId);
+    if (!user) throw new UserNotFoundException();
+
     return await this.authService.signIn(user);
   }
 
